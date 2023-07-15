33a9dc0dee0dfc70c47db1ffd4d6575cb0f9f1b8
Erdem Emiroğlu <erdem.emiroglu@ocrlabs.com>
2023-07-15 14:35:18 +0300

    fix axios config

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/' || '/', 
-  timeout: 5000,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-  },
+  baseURL: 'http://localhost:3000/',
 });
